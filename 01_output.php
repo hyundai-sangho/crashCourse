a17c3829f686fa56d578fe17d00a8d43deb9ccce
조상호 <hyundai_sangho@naver.com>
2022-09-28 19:28:54 +0900

    echo, print_r, var_dump, var_export 사용법

---
+++ 01_output.php
@@ -0,0 +1,27 @@
+<?php
+echo '123';
+echo [1, 2, 3];
+echo '<br>';
+var_dump([1, 2, 3]);
+echo '<br>';
+print_r([1, 2, 3]);
+echo '<br>';
+var_export([1, 2, 3]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+
+  <body>
+    <h1><?= 'Post One' ?></h1>
+  </body>
+
+</html>
